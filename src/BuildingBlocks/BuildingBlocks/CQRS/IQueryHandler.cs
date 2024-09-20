30c758541665269d297babad27dcf51623c4d3a8
Dragos <dragos.boboluta@yahoo.com>
2024-09-20 09:35:38 -0700

    abstract MediatR

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<in TCommand>
+        : IRequestHandler<TCommand, Unit>
+        where TCommand : ICommand<Unit>
+    {
+
+    }
+    public interface IQueryHandler <in TQuery,Tresponse>
+        :IRequestHandler<TQuery,Tresponse>
+        where TQuery : IQuery<Tresponse>
+        where Tresponse : notnull
+    {
+
+    }
+}
